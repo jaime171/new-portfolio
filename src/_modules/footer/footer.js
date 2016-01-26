8787ee6ccd67f4382265d2847b7bddd944dac582
jaimem <jaimem@redman.com>
2016-01-26 02:38:38 -0600

    Other bugs fixed

--- src/_modules/footer/footer.js
+++ src/_modules/footer/footer.js
@@ -5,7 +5,6 @@ var Footer = function() {
   var $ = require('jquery');
   var windowWidth = $(window).width();
   if (windowWidth >= 641) {
-    console.log("True")
     $('.social-bar li').hover(function(){
       $(this).animate({
         "margin-right": '-10'
--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -1,12 +1,17 @@
 'use strict';
 var Header = function() {
   var $ = require('jquery');
+  var body = document.body,
+      socialBar = document.getElementById('social-bar'),
+      menu = document.getElementById('menu');
   $('#triggerDown').on('click', function(){
     $('#menu').slideDown({width:'toggle'},350);
+    body.style.overflow = "hidden";
+    menu.style.zIndex = 99;
   });
   $('#triggerUp').on('click', function(){
-    $('#menu').slideUp('slow');
+    $('#menu').slideUp({width:'toggle'},350);
+    body.style.overflow = "auto";
   });
 };
-
 module.exports = Header;
